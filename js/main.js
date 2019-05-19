cd2edf16a9e79608efd7a827e3ce16ed68306fc9
Logan Stone <logan.stone20@gmail.com>
2019-05-19 04:49:33 -0230

    fixed jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $("body").fadeIn(1000);
+    $("body").fadeIn(1500);
+
      $('#resume-button,#email,#top').each(function() {
 
         $(this).data('original-size', $(this).css('fontSize'));
@@ -16,6 +17,30 @@ $(document).ready(function () {
           var originalSize = $(this).data('original-size');
           $(this).animate({
             fontSize: originalSize
+          }, 200);
+        });
+    });
+
+     $('img').each(function() {
+
+        $(this).data('original-width', $(this).css('width'));
+        $(this).data('original-height', $(this).css('height'));
+
+        $(this).hover(function() {
+
+            $(this).animate({
+                height: "5em",
+                width: "5em"
+            }, 200);
+
+        });
+
+        $(this).mouseout(function () {
+          var originalWidth = $(this).data('original-width');
+          var originalHeight = $(this).data('original-height');
+          $(this).animate({
+            height: originalHeight,
+            width: originalWidth
           }, 200);
         });
     });
