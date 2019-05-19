726cd863b51bc7d9027c3a28a81fd0aeae946a7a
Logan Stone <logan.stone20@gmail.com>
2019-05-19 03:38:53 -0230

    woo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
     $("body").fadeIn(1000);
+     $('#resume-button,#email,#top').each(function() {
+
+        $(this).data('original-size', $(this).css('fontSize'));
+
+        $(this).mouseenter(function() {
+
+            $(this).animate({
+                fontSize: "30px"
+            }, 200);
+
+        });
+
+        $(this).mouseout(function () {
+          var originalSize = $(this).data('original-size');
+          $(this).animate({
+            fontSize: originalSize
+          }, 200);
+        });
+    });
 });
