b132bc86d38f2988e8f807ae76820a660a17f5cd
Logan Stone <logan.stone20@gmail.com>
2019-08-18 19:27:19 -0230

    fixed hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $("body").fadeIn(1500);
+
+    $(".nav-item").click(function() {
+      $(".navbar-toggler").attr("aria-expanded", false);
+      $(".navbar-toggler").addClass("collapsed");
+      $(".navbar-collapse.collapse.show").removeClass("show");
+    })
 });
